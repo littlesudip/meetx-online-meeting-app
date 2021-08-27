ab3e5aa5d57bbca45f8069d362733adbbc1543e2
Anushka Azad <68438635+Anushkaazad@users.noreply.github.com>
2021-08-27 23:58:43 +0600

    Add_user_and_set_connection

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,18 +1,77 @@
-var MyApp= (function(){
+var AppProcess= function () {
+
+
+   var iceConfiguration = {
+       iceServers:[
+           {
+               urls:"stun:stun.l.google.com:19302",
+           },
+           {
+            urls:"stun:stun.l.google.com:19302",
+        },
+       ]
+   }
+
+  function setConnetion(connid){
+   var connection = new RTCPeerConnection(iceConfiguration);    
+
+  }
+
+  return{
+      setNewConnetion: async function(connid){
+          await setConnetion(connid);
+      },
+  };
+
+
+};
+var MyApp = (function()  { 
+
+    var socket = null;
+    var user_id = "";
+    var meeting_id = "";
     function init (uid,mid){
+        user_id = uid;
+        meeting_id = "";
         event_process_for_signaling_server();
     }
     var socket=null;
     function event_process_for_signaling_server(){
         socket = io.connect();
         socket.on ("connect",()=>{
-            alert("socket connected to client side");
+            if(socket.connected){
+
+                if(user_id != "" && meeting_id != ""){
+                    socket.emit("userconnect",{
+                        displayName: user_id,
+                        meetingid: meeting_id
+                    });
+                }
+            }
+            
 
         });
+
+        socket.on("inform_other_about_me", function(data){
+           addUser(data.other-user_id, data.connId); 
+           AppProcess.setNewConnection(data.connId); 
+        });
     }
+    function addUser(other_user_id,connId){
+        var newDivId = $("#otherTemplate").clone();
+        newDivId = newDivId.attr("id",connId).addClass("other");
+        newDivId.find("h2").text(other_user_id);
+        newDivId.find("video").attr("id","v_"+connId);
+        newDivId.find("audio").attr("id","a_"+connId);
+        newDivId.show();
+        $("#divUsers").append(newDivId);
+
+    }
+
+
     return{
         _init:function(uid,mid){
             init(uid,mid);
         },
     };
-})();
+});
