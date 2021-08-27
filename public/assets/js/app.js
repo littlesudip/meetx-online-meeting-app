4efe4f8138c446573f431e901630701609dfa3d3
EkhtiarUddin <uddinikhtiar6@gmail.com>
2021-08-27 18:25:33 +0600

    express server & socket setup at client site

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,18 @@
-var MyApp = function(){
-    
-    function init()(uid,mid) {
+var MyApp= (function(){
+    function init (uid,mid){
+        event_process_for_signaling_server();
     }
-    return {
-        _init: function(uid,mid){
+    var socket=null;
+    function event_process_for_signaling_server(){
+        socket = io.connect();
+        socket.on ("connect",()=>{
+            alert("socket connected to client side");
+
+        });
+    }
+    return{
+        _init:function(uid,mid){
             init(uid,mid);
         },
     };
-};
+})();
