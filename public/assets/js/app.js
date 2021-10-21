54007f7c5cf188b098489686e9c252cdbff2f2d7
Sudip Ghose <18101094@uap-bd.edu>
2021-10-21 21:33:01 +0600

    Leave Meeting Updated.
    Button And Function

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -515,6 +515,30 @@ var MyApp = (function () {
         .css({
           display: "block",
         })  
+        .html(
+          '<div class="top-box align-vertical-middle profile-dialogue-show"> <h4 class="mt-3" style="text-align:center;color:white;">Leave Meeting</h4> <hr> <div class="call-leave-cancel-action d-flex justify-content-center align-items-center w-100"> <a href="/action.html"><button class="call-leave-action btn btn-danger mr-5">Leave</button></a> <button class="call-cancel-action btn btn-secondary">Cancel</button> </div> </div>'
+        );
+    });
+    $(document).mouseup(function (e) {
+      var container = new Array();
+      container.push($(".top-box-show"));
+      $.each(container, function (key, value) {
+        if (!$(value).is(e.target) && $(value).has(e.target).length == 0) {
+          $(value).empty();
+        }
+      });
+    });
+    $(document).mouseup(function (e) {
+      var container = new Array();
+      container.push($(".g-details"));
+      container.push($(".g-right-details-wrap"));
+      $.each(container, function (key, value) {
+        if (!$(value).is(e.target) && $(value).has(e.target).length == 0) {
+          $(value).hide(300);
+        }
+      });
+    });
+    
   return {
     _init: function (uid, mid) {
       init(uid, mid);
